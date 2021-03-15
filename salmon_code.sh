9534b0399d4c3dfc0ed7e3fbfb4edf02008e5cc4
PaarthParekh <55130298+PaarthParekh@users.noreply.github.com>
2021-03-15 14:48:17 -0400

    Added in comments

--- salmon_code.sh
+++ salmon_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# for all the files (17 to 21) in the directory run the salmon quantification command to obtain the counts for each transcript  
 for fn in ./fastq_files/SRR10395{17..21};
 do
+# this variable is to print to std out, what sample is currently running for salmon quantification
 samp=`basename ${fn}`
 echo "Processing sample ${samp}"
 salmon quant -i transcript_index -l A \
